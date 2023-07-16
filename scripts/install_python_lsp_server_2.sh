24cc8b2a2d9de36a56a8224a4b830910d8427cdc
KubaTaba1uga <jakub@buczynski.net.pl>
2023-07-16 12:33:38 +0200

    save progress

--- scripts/install_python_lsp_server_2.sh
+++ scripts/install_python_lsp_server_2.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y virtualenv
 virtualenv $EMACS_VIRTUALENV_PATH
 
 # install lsp server and extensions
-$EMACS_PYTHON_PATH -m pip install python-lsp-server==1.7.4 python-lsp-black python-lsp-ruff pylsp-mypy python-lsp-isort
+$EMACS_PYTHON_PATH -m pip install python-lsp-server==1.7.3 python-lsp-black python-lsp-ruff pylsp-mypy python-lsp-isort
 
 # install python packages
 $EMACS_PYTHON_PATH -m pip install black ruff mypy isort
