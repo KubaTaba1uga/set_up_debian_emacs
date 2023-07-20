6fd7d5bdee020e351cc34f611b771f63f8d25657
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-07-20 21:17:20 +0200

    Update install_clang_lsp_server_3.sh

--- scripts/install_clang_lsp_server_3.sh
+++ scripts/install_clang_lsp_server_3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # install language server
-sudo apt-get install clangd-15
+sudo apt-get install -y clangd-15
 
 # install tool for generating
 # project's commands db, example:
 #  https://emacs-lsp.github.io/lsp-mode/tutorials/CPP-guide/#project-setup
-sudo apt-get install bear
+sudo apt-get install -y bear
 
